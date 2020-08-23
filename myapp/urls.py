030dc6ce2f5f83b812cd9f150fcf4f3a59ecea0f
Devansh Sharma <ds8611@bennett.edu.in>
2020-08-24 05:00:19 +0530

    front-end

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-  path('', views.home, name = 'home'),
+    path('admin/', admin.site.urls),
+    path('', views.home, name = 'home'),
+    path('new_search', views.new_search, name="new_search")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
 def home(request):
   return render(request, 'base.html')
+
+
+def new_search(request):
+  search = request.POST.get('search')
+  stuff_for_frontend={
+    'search': search, 
+  }
+  return render(request, 'myapp/new_search.html', stuff_for_frontend)
