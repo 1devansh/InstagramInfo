ded2b29c6a733cd5d4dc5704af6da7e3c1c4d10d
Devansh Sharma <ds8611@bennett.edu.in>
2020-08-23 07:40:13 +0530

    initial commit

---
+++ myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Search(models.Model):
+  search = models.CharField(max_length=500)
+  created = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.search
+  class Meta:
+    verbose_name_plural = 'searches'
